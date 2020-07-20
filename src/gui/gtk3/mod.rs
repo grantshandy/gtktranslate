6d1fe8977f002c5cdc0d55dc9dd434239894bd6f
DefunctLizard <grantshandy@gmail.com>
2020-07-20 14:00:46 -0600

    add scrollbar and remove errors

--- src/gui/gtk3/mod.rs
+++ src/gui/gtk3/mod.rs
@@ -3,29 +3,30 @@ use std::process::Command;
 use std::str;
 use gtk::AboutDialog;
 use gtk::License::Gpl30;
-use glib_sys::g_free;
+use gtk::Inhibit;
+
+#[allow(unused_mut)]
 
 pub fn launch() {
 	gtk::init().unwrap_or_else(|_| panic!("GTK could not start!"));
 
-//	Declare windows and import .ui file.
+//	Import app_window.ui and set the app_window as main window.
 	let builder = gtk::Builder::new_from_string(include_str!("app_window.ui"));
 	let main_window: gtk::Window = builder.get_object("app_window").unwrap();
-	let about_window = AboutDialog::new();
-	let about_button: gtk::Button = builder.get_object("about-button").unwrap();
-	let translate_button: gtk::Button = builder.get_object("translate-button").unwrap();
-	let input_lang: gtk::ComboBoxText = builder.get_object("input-lang").unwrap();
-	let output_lang: gtk::ComboBoxText = builder.get_object("output-lang").unwrap();
 
-//	execute about_button function.
+//	Import translate, about and verbose buttons from builder.
+	let translate_button: gtk::Button = builder.get_object("translate-button").unwrap();
+	let about_button: gtk::Button = builder.get_object("about-button").unwrap();
+	let verbose_button: gtk::ToggleButton = builder.get_object("verbose-button").unwrap();
+	
+//	Execute about_button function.
 	about_button.connect_clicked(move |_| {
 		println!("Starting About Dialog");
-
-//		Set AboutDialog Preferences.
+		let about_window = AboutDialog::new();
 		about_window.set_website_label(Some("gtktranslate"));
-		about_window.set_website(Some("https://skylinecc.github.io"));
-		about_window.set_website_label(Some("Skyline Coding Club Website"));
-    	about_window.set_comments(Some("A GTK3 Google Translate Wrapper"));
+		about_window.set_website(Some("https://github.com/skylinecc/gtktranslate"));
+		about_window.set_website_label(Some("Gtktranslate Project Page"));
+		about_window.set_comments(Some("A GTK3 Google Translate Wrapper"));
 		about_window.set_copyright(Some("Copyright © 2020 Skyline Coding Club"));
 		about_window.set_license_type(Gpl30);
 		about_window.set_wrap_license(false);
@@ -37,75 +38,64 @@ pub fn launch() {
 			"Alex Ikeda",
 			"Evan Ikeda",
 		]);
-		about_window.show_all();
+	
+    		about_window.show_all();
 	});
-
-//	execute translate_button function.
-	translate_button.connect_clicked(move |_| {
+	
+	
+	
+//	Execute translate_button function.
+        translate_button.connect_clicked(move |_| {
         
-//		Unwrap input buffer and do some utf-8 coding gymnastics. Seriously, this is stupid, don't do this.
-		let mut inputtext: gtk::TextView = builder.get_object("input-text").unwrap();
-		let mut inputbuffer: gtk::TextBuffer = inputtext.get_buffer().unwrap();
-		let (start,end) = inputbuffer.get_bounds();
-		let text = inputbuffer.get_text(&start,&end,false).unwrap();
-		let input = text.as_str();
+        let mut inputtext: gtk::TextView = builder.get_object("input-text").unwrap();
+        let mut inputbuffer: gtk::TextBuffer = inputtext.get_buffer().unwrap();
+        let (start,end) = inputbuffer.get_bounds();
+        let text = inputbuffer.get_text(&start,&end,false).unwrap();
+        let text_str = text.as_str();
         
-//	    	Import "trans" command for translations, and make it brief.
-		let mut command = Command::new("gawk -f <(curl -Ls git.io/translate)");
-		let mut cmd = &mut command;
-	        cmd = cmd.arg("-b");
+        let mut command = Command::new("/usr/bin/trans");
+        let mut cmd = &mut command;
+        cmd = cmd.arg("--no-ansi");
 
-//		Get Input and Output Languages from ComboBox.
-        	let mut active_input = input_lang.get_active_text().unwrap();
-        	let mut active_input_str = active_input.as_str();
+	let source_language = "en";
+        let destination_language = "es";
+	
+	if !verbose_button.get_active() {
+	        cmd = cmd.arg("-b")
+	}
 
-		let mut active_output = output_lang.get_active_text().unwrap();
-		let mut active_output_str = active_output.as_str();	
-
-//		Convert fancy input and output names into simple 2 letter names
-		
-		let mut source_language = "";
-		let mut destination_language"";
-
-		if active_input_str == "English" {
-			let mut source_language = "en";
-			println!("source language: English");
-		};
-       	
-      		if active_output_str == "French" {
-			let mut destination_language = "fr";
-			println!("Destination language set: French");
-		};
-       	
-       	if active_output_str == "Spanish" {
-			let mut destination_language = "es";
-			println!("Destination language set: Spanish");
-		};
-
-//	    	If the source and destination languages are entered, set them as parameters to "trans".
-		if !source_language.is_empty() && !destination_language.is_empty() {
-			cmd = cmd.arg(format!("{}:{}", source_language, destination_language));
-		}
+        if !source_language.is_empty() && !destination_language.is_empty() {
+            cmd = cmd.arg(format!("{}:{}", source_language, destination_language));
+        }
         
-//	    	Do more utf-8 coding gymnastics just to get our output from "trans" back into the output text box.
-		let command = cmd.arg(input);
-		let output = cmd.output().unwrap().stdout;
-		let output = str::from_utf8(&output).unwrap();
-		let mut outputtext: gtk::TextView = builder.get_object("output-text").unwrap();
-		let mut outputbuffer: gtk::TextBuffer = outputtext.get_buffer().unwrap();
-		outputbuffer.set_text(output);
-       
-//	    	Print our input -> output into the console.
-		println!("{} -> {}", input, output);       	
+        let command = cmd.arg(text_str);
+        let output = cmd.output().unwrap().stdout;
+        let output = str::from_utf8(&output).unwrap();
+        let mut outputtext: gtk::TextView = builder.get_object("output-text").unwrap();
+        let mut outputbuffer: gtk::TextBuffer = outputtext.get_buffer().unwrap();
+	
+//	outputtext.scroll_to_mark(
+//		&self,
+//		mark: &p,
+//		within_margin: f64,
+//		use_align: bool,
+//		xalign: f64
+//		yalign: f64
+//	);
 
-	});
+        outputbuffer.set_text(output);
+
+        dbg!(output);
+    	});
 
 	main_window.show_all();
+
+	//... and to kill the event...
 
 	main_window.connect_delete_event(|_, _| {
 		gtk::main_quit();
 		Inhibit(false)
-	});
+    	});
     
 	gtk::main();
 }
